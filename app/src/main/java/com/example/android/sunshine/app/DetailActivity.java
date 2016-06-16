f9a0010f81e5f813b23a673c8ed0b478d700770c
syed samiuddin <ssamiuddin007@gmail.com>
2016-06-16 15:45:47 +0530

    Added SettingsActivity class,and Edited Manifest to support another activity , edited Strings.xml file

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -44,7 +44,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+           startActivity(new Intent(this,SettingsActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
